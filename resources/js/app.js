112a0fd701cf2f285ed943fe6b8b8e7513207049
souanyyassine <souanyyassine1@gmail.com>
2023-04-15 02:19:38 +0000

    commit

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import welcome from "./welcome.vue";
+
+createApp(welcome).mount("#app");
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>llaravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+
+    </head>
+    <body class="antialiased">
+        <div id="app"></div>
+    </body>
+</html>
